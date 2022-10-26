ae37636c55640d1d9dccecdba685fc24c2e80bc5
Gildas Rouault <g.rouault@groupe-pomona.fr>
2022-10-26 09:29:32 +0200

    Test - BookRepositoryTest

---
+++ notes/todo.md
@@ -0,0 +1,9 @@
+* Objet de domaine mais pas d'objets de présentation
+  * on a des champs transient dans le bean pour la présentation
+  * pas de hashcode ni equals
+  * pas de séquence
+* Validation par Spring des données entrantes
+  * collision avec la validation hibernate ???
+  ==> annotation sur les bean Java (annotation Spring et Hibernate)
+* Enum : non géré par hibernate
+* Hibernate: les mappings sont fait à minima
--- src/test/java/com/udemy/sharebook/repository/BookRepositoryTest.java
+++ src/test/java/com/udemy/sharebook/repository/BookRepositoryTest.java
@@ -1,2 +1,27 @@
-package com.udemy.sharebook.repository;public class BookRepositoryTest {
+package com.udemy.sharebook.repository;
+
+import com.udemy.sharebook.book.Category;
+import com.udemy.sharebook.book.CategoryRepository;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class BookRepositoryTest {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    public void getAllCategories(){
+        List<Category> categories = categoryRepository.findAll();
+        assertThat(categories).as("Les catégories n'ont pas été chargées correctement").hasSize(7);
+    }
+
 }
